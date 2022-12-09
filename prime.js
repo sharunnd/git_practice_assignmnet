2cebdf3945aa9e6f6c702b7a94bd355ac5359b2e
Your Name <sharunnd11@gmail.com>
2022-12-09 14:19:32 +0530

    code added

---
+++ prime.js
@@ -0,0 +1,13 @@
+let num = 13;
+let count = 0;
+for(let i = 0;i <= num; i++){
+if(num % i ==){
+count++;
+}
+}
+if(count == ){
+console.log("it is a prime");
+}
+else{
+console.log("it is not a prime");
+}
