fe3d6ad818b756ba9e03a271d235cafaa1028cda
unknown <pariharansh123@.com>
2022-12-10 20:46:14 +0530

    code checked

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 let num = 13;
 let count = 0;
-for(let i = 0;i <= num; i++){
-if(num % i ==){
+for(let i = 1;i <= num; i++){
+if(num % i ==0){
 count++;
 }
 }
-if(count == ){
+if(count == 2){
 console.log("it is a prime");
 }
 else{
